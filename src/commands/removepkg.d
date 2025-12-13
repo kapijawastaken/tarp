164f5948545c1dd0f76642313cc015ac4f081b6b
kapijawastaken <gabriellsimic5@gmail.com>
2025-12-13 13:56:27 +0100

    removing pkgs works now!

--- src/commands/removepkg.d
+++ src/commands/removepkg.d
@@ -8,7 +8,8 @@ string removepkg(string[] args) {
   if (args.length == 0) {
     return help();
   } else {
-  string packageFile = args[0];
-  return executeShell("removepkg " ~ packageFile).output;
+    string packageFile = args[0];
+    return executeShell("removepkg " ~ packageFile).output;
   }
+
 }
