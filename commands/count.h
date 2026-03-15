de1914ad5eff16dfc9b9dc7493f34ed4e040ce37
Gabriell <ri@tankpad.>
2026-03-15 18:47:17 +0100

    fixed count()

--- commands/count.h
+++ commands/count.h
@@ -1,13 +1,11 @@
 #include <dirent.h>
-#include <stdio.h>
 #include <stdlib.h>
 int count() {
   struct dirent **entries;
   int n = scandir("/var/lib/pkgtools/packages", &entries, NULL, NULL);
-  printf("%d\n", n);
   for (int i = 0; i < n; i++) {
     free(entries[i]);
   }
   free(entries);
-  return n;
+  return n - 2; // n - 2 because of . and ..
 }
