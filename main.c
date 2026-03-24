d27be0a86633371620ca9e84cb7695a10d070b85
kapijawastaken <gabriellsimic5@gmail.com>
2026-03-24 20:49:27 +0100

    made main() work, doesnt do much yet

--- main.c
+++ main.c
@@ -2,18 +2,26 @@
 #include <string.h>
 #include "commands/help.h"
 
-int main(char **args) {
-  if (sizeof(args) / sizeof(*args) <= 1) {
-    printf(help());
-  } 
+int main(int argc, char **argv) {
+  if (argc <= 1) {
+    printf("%s", help());
+    return 1;
+  }
+  else if (strcmp(argv[1], "h") == 0 || strcmp(argv[1], "help") == 0) {
+    printf("%s", help());
+  }
+  else {
+    printf("%s", help());
+    return 1;
+  }
 
   /*
-  char **names = list();
-  for (int i = 0; names[i] != NULL; i++) {
+    char **names = list();
+    for (int i = 0; names[i] != NULL; i++) {
     printf("%s\n", names[i]);
     free(names[i]);
-  }
-  free(names);
+    }
+    free(names);
   */
   return 0;
 }
