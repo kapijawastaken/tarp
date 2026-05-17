4ba84b8a9e1a26e37c54a7ceb9c27208f50433f6
kapijawastaken <gabriellsimic5@gmail.com>
2026-05-17 13:44:27 +0200

    created installpkg(), the local part of the c version is finally done!

--- commands/help.h
+++ commands/help.h
@@ -2,7 +2,7 @@ char *help() {
   char *helptext = "Usage: tarp <command>\n" 
                 "Available commands:\n" 
                 "install, i <package>   Installs a local or remote package\n" 
-                "list, l                Lists installed packages\n" 
+                "list, l                Lists installed packages (It's recommended to pipe this into a pager like less)\n" 
                 "count, c               Shows the amount of installed packages\n" 
                 "remove, r <package>    Removes a package\n" 
                 "update, u              Updates the package list\n" 
---
+++ commands/installpkg.h
@@ -0,0 +1,36 @@
+char *installpkg(int argc, char **argv) {
+  if (argc <= 0) {
+    // main() frees this function, so we need it on the heap.
+    char *out;
+    asprintf(&out, "%s", help());
+    return out;
+  }
+  else {
+    char *cmd;
+    asprintf(&cmd, "installpkg");
+
+    for (int i = 0; i < argc; i++) {
+      char *tmp;
+      asprintf(&tmp, "%s %s", cmd, argv[i]);
+      free(cmd);
+      cmd = tmp;
+    }
+    
+    char *result;
+    asprintf(&result, "%s", ""); // asprintf needs at least 2 args
+    int c;
+    FILE *fp = popen(cmd, "r");
+    free(cmd);
+    
+    while ((c = fgetc(fp)) != EOF) {
+      // here we append the data stream c to result.
+      char *tmp;
+      asprintf(&tmp, "%s%c", result, c);
+      free(result);
+      result = tmp;
+    }
+    
+    pclose(fp);
+    return result;
+  }
+}
