90e094f174502837d8c6066c4b0949156ff5a343
kapijawastaken <gabriellsimic5@gmail.com>
2026-08-07 23:43:28 +0200

    implemented update() fully

--- commands/update.h
+++ commands/update.h
@@ -1,26 +1,70 @@
-char *update() {
+int update() {
   char **mirrors = pkgmirrors();
-  if (mirrors == NULL) {
-    return "You don't have any mirrors set!\n"
-      "Go and do that by uncommenting one"
-      "or more mirrors in /etc/tarp/mirrors.";
-  }
+  char *checksum_path = NULL, *signature_path = NULL;  
+  char repoid[9];
 
-  char *checksum_path = NULL;  
+  if (mirrors == NULL) {
+    fprintf(stderr, "%s\n",
+	    "You don't have any mirrors set!\n"
+	    "Go and do that by uncommenting one "
+	    "or more mirrors in /etc/tarp/mirrors.");
+    return 1;
+  }
+  
   for (int i = 0; mirrors[i] != NULL; i++) {
+    snprintf(repoid, 9, "%08x", fnv1a(mirrors[i]));
+
     if (strncmp(mirrors[i], "https://", 8) != 0 &&
         strncmp(mirrors[i], "http://", 7) != 0 &&
 	strncmp(mirrors[i], "ftp://", 6) != 0) {
-      return "This is a local repo, running tarp update is unneccessary.";
+      fprintf(stderr, "%s is a local repo, running "
+	      "tarp update is unneccessary.\n", repoid);
+      return 1;
     }
 
     asprintf(&checksum_path, "%s/CHECKSUMS.md5", tmpdir(mirrors[i]));
     if (access(checksum_path, F_OK) == -1) {
-      // nothing yet
+      char *checksum_url = NULL;
+      asprintf(&checksum_url, "%s/CHECKSUMS.md5", mirrors[i]);
+      int choice;
+      
+    checksum:
+      if (download(checksum_url, checksum_path) == 1) {
+	fprintf(stderr, "Checksum download of %s failed!\n", repoid);
+	printf("Retry? [Y/n] ");
+	fflush(stdout);
+	choice = getchar();
+	int c;
+	while ((c = getchar()) != '\n' && c != EOF); // discard rest of line
+	if (choice == 'y' || choice == 'Y' || choice == '\n') { goto checksum; }
+      }
+      free(checksum_url);
     }
     free(checksum_path);
+
+
+    asprintf(&signature_path, "%s/CHECKSUMS.md5.asc", tmpdir(mirrors[i]));
+    if (access(signature_path, F_OK) == -1) {
+      char *signature_url = NULL;
+      asprintf(&signature_url, "%s/CHECKSUMS.md5.asc", mirrors[i]);
+      int choice;
+      
+    signature:
+      if (download(signature_url, signature_path) == 1) {
+	fprintf(stderr, "Signature download of %s failed!\n", repoid);
+	printf("Retry? [Y/n] ");
+	fflush(stdout);
+	choice = getchar();
+	int c;
+	while ((c = getchar()) != '\n' && c != EOF); // discard rest of line
+	if (choice == 'y' || choice == 'Y' || choice == '\n') { goto signature; }
+      }
+      free(signature_url);
+    }
+    free(signature_path);
+
   }
-  return; // nothing yet either
+  return 0;
 }
 
 char *tmpdir(char *str) {
