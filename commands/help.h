5372894fb1d528967d20a0ad758b176dcc8230d5
kapijawastaken <gabriellsimic5@gmail.com>
2026-08-08 20:42:44 +0200

    fixed misalignment in help menu

--- commands/help.h
+++ commands/help.h
@@ -2,7 +2,7 @@ int help() {
   char *helptext = "Usage: tarp <command>\n" 
     "Available commands:\n" 
     "install, i <package>   Installs a local or remote package\n"
-    "repoid, ri <repoID>      Prints the repo belonging to the repoID\n"
+    "repoid, ri <repoID>    Prints the repo belonging to the repoID\n"
     "list, l                Lists installed packages "
     "(It's recommended to pipe this into a pager like less)\n"
     "count, c               Shows the amount of installed packages\n"
